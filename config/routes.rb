0c7cb085d9ac0e797d9641e56d1eaff374b843ca
EC2 Default User <141564706+Satoshi-1217@users.noreply.github.com>
2023-08-20 12:29:28 +0000

    [Finish]課題2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # トップ画面
-  root : => 'homes#top'
+  root :to => 'homes#top'
   # # 新規投稿
   # # get 'books/new'
   # post 'books' => 'books#create'
@@ -13,8 +13,8 @@ Rails.application.routes.draw do
   # patch 'books/:id' => 'books#update', as: 'update_book'
   # # 削除
   # delete 'books/:id' => 'books#destroy', as: 'destroy_book'
-  
+
   resources :books
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
